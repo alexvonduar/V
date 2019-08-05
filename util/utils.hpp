1711c37a01fdc9de199ed8e44557752633426064
Alex Vonduar <alexvonduar@msn.com>
2019-08-05 18:48:15 +0800

    add missing headers

---
+++ util/utils.hpp
@@ -0,0 +1,273 @@
+#ifndef _UTILS_HPP_
+#define _UTILS_HPP_
+
+#include <opencv2/opencv.hpp>
+
+namespace VUtils
+{
+
+template <typename _Tp>
+static inline bool isContourConvex_(const cv::Point_<_Tp> *p, int n)
+{
+    cv::Point_<_Tp> prev_pt = p[(n - 2 + n) % n];
+    cv::Point_<_Tp> cur_pt = p[n - 1];
+
+    _Tp dx0 = cur_pt.x - prev_pt.x;
+    _Tp dy0 = cur_pt.y - prev_pt.y;
+    int orientation = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        _Tp dxdy0, dydx0;
+        _Tp dx, dy;
+
+        prev_pt = cur_pt;
+        cur_pt = p[i];
+
+        dx = cur_pt.x - prev_pt.x;
+        dy = cur_pt.y - prev_pt.y;
+        dxdy0 = dx * dy0;
+        dydx0 = dy * dx0;
+
+        // find orientation
+        // orient = -dy0 * dx + dx0 * dy;
+        // orientation |= (orient > 0) ? 1 : 2;
+        orientation |= (dydx0 > dxdy0) ? 1 : ((dydx0 < dxdy0) ? 2 : 3);
+        if (orientation == 3)
+            return false;
+
+        dx0 = dx;
+        dy0 = dy;
+    }
+
+    return true;
+}
+
+template <typename _Tp>
+static inline bool isContourConvex_(const cv::Vec<_Tp, 2> *p, int n)
+{
+    auto prev_pt = p[(n - 2 + n) % n];
+    auto cur_pt = p[n - 1];
+
+    _Tp dx0 = cur_pt[0] - prev_pt[0];
+    _Tp dy0 = cur_pt[1] - prev_pt[1];
+    int orientation = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        _Tp dxdy0, dydx0;
+        _Tp dx, dy;
+
+        prev_pt = cur_pt;
+        cur_pt = p[i];
+
+        dx = cur_pt[0] - prev_pt[0];
+        dy = cur_pt[1] - prev_pt[1];
+        dxdy0 = dx * dy0;
+        dydx0 = dy * dx0;
+
+        // find orientation
+        // orient = -dy0 * dx + dx0 * dy;
+        // orientation |= (orient > 0) ? 1 : 2;
+        orientation |= (dydx0 > dxdy0) ? 1 : ((dydx0 < dxdy0) ? 2 : 3);
+        if (orientation == 3)
+            return false;
+
+        dx0 = dx;
+        dy0 = dy;
+    }
+
+    return true;
+}
+
+static inline bool isContourConvex(cv::InputArray _contour)
+{
+    cv::Mat contour = _contour.getMat();
+    int total = contour.checkVector(2), depth = contour.depth();
+    CV_Assert(total >= 0 && (depth == CV_32F || depth == CV_32S));
+
+    if (total == 0)
+        return false;
+
+    return depth == CV_32S ? isContourConvex_(contour.ptr<cv::Point>(), total) : isContourConvex_(contour.ptr<cv::Point2f>(), total);
+}
+
+template <typename _Tp>
+static inline bool is_convex_polygon(const std::vector<cv::Point_<_Tp>> &polygon)
+{
+    assert(polygon.size() >= 3);
+    auto p = &polygon[0];
+    auto N = polygon.size();
+    return isContourConvex_(p, N);
+}
+
+template <typename _Tp, std::size_t N>
+static inline bool is_convex_polygon(const std::array<cv::Point_<_Tp>, N> &polygon)
+{
+    assert(polygon.size() >= 3);
+    auto p = &polygon[0];
+    return isContourConvex_(p, N);
+}
+
+template <typename _Tp>
+static inline bool is_convex_polygon(const std::vector<cv::Vec<_Tp, 2>> &polygon)
+{
+    assert(polygon.size() >= 3);
+    auto p = &polygon[0];
+    auto N = polygon.size();
+    return isContourConvex_(p, N);
+}
+
+template <typename _Tp, std::size_t N>
+static inline bool is_convex_polygon(const std::array<cv::Vec<_Tp, 2>, N> &polygon)
+{
+    assert(polygon.size() >= 3);
+    auto p = &polygon[0];
+    return isContourConvex_(p, N);
+}
+
+static inline float identity_similarity(const cv::Mat &hom)
+{
+    std::vector<cv::Point2f> input{cv::Point2f{0., 0.}, cv::Point2f{640., 0.}, cv::Point2f{640., 640.}, cv::Point2f{0., 640.}};
+    std::vector<cv::Point2f> output(input.size());
+    cv::perspectiveTransform(input, output, hom);
+    if (is_convex_polygon(output))
+    {
+        cv::Point2f offset = output[0];
+        for (auto &pt : output)
+        {
+            pt -= offset;
+        }
+        std::vector<cv::Point2f> points;
+        auto area = cv::intersectConvexConvex(input, output, points);
+        return area;
+    }
+    else
+    {
+        return -1.;
+    }
+}
+
+static inline bool morphological_check(const cv::Mat &hom, const cv::Rect &_rect)
+{
+    auto rect = _rect;
+    if (rect.area() == 0)
+    {
+        rect = cv::Rect(0, 0, 640, 640);
+    }
+    std::vector<cv::Point2f> input{
+        cv::Point2f{float(rect.x), float(rect.y)},
+        cv::Point2f{float(rect.x + rect.width), float(rect.y)},
+        cv::Point2f{float(rect.x + rect.width), float(rect.y + rect.height)},
+        cv::Point2f{float(rect.x), float(rect.y + rect.height)}};
+    std::vector<cv::Point2f> output(input.size());
+    cv::perspectiveTransform(input, output, hom);
+    /*
+    std::cout << "hom: " << hom << std::endl;
+    for (int i = 0; i < input.size(); ++i) {
+        ZLOGI("[%f, %f] -> [%f, %f]\n", input[i].x, input[i].y, output[i].x, output[i].y);
+    }
+    */
+    /*
+    cv::Point2f offset = output[0];
+
+    for (auto& pt : output) {
+        pt -= offset;
+        //ZLOGI("transformed %f %f\n", pt.x, pt.y);
+    }
+    */
+    bool is_convex = is_convex_polygon(output);
+    auto rrect = cv::minAreaRect(output);
+    auto aspect_ratio = rrect.size.width > rrect.size.height ? (rrect.size.width / rrect.size.height) : (rrect.size.height / rrect.size.width);
+    auto valid_aspect_ratio = aspect_ratio < 20;
+    auto valid_scale = false;
+    auto valid_offset = false;
+    if (is_convex)
+    {
+        auto original_area = rect.area();
+        auto warped_area = rrect.size.area();
+        auto scale = (warped_area > original_area) ? (warped_area / original_area) : (original_area / warped_area);
+        printf("area: %d %f scale: %f\n", original_area, warped_area, scale);
+        valid_scale = scale < 49;
+        auto offset_x = std::abs(rrect.center.x);
+        auto offset_scale = offset_x / rect.width;
+        auto valid_offset_x = offset_scale < 80;
+        auto offset_y = std::abs(rrect.center.y);
+        offset_scale = offset_y / rect.height;
+        printf("offset: %f %f\n", offset_x, offset_y);
+        auto valid_offset_y = offset_scale < 80;
+        valid_offset = valid_offset_x and valid_offset_y;
+        if (!valid_scale or !valid_offset)
+        {
+            printf("scale: %f offset: [%f %f]\n", scale, offset_x, offset_y);
+        }
+    }
+
+    auto valid = is_convex and valid_aspect_ratio and valid_scale and valid_offset;
+    //if (!valid) {
+    printf("is convex: %d aspect ratio: %f[valid %d] scale: %d offset: %d\n", is_convex, aspect_ratio, valid_aspect_ratio, valid_scale, valid_offset);
+    //}
+    return valid;
+}
+
+static inline auto find_best_identity(const std::vector<cv::Mat> &hom) -> decltype(hom.size())
+{
+    auto n = hom.size();
+    decltype(n) idx = 0;
+    float max_area = 0;
+    for (decltype(n) i = 0; i < n; ++i)
+    {
+        auto area = identity_similarity(hom[i]);
+        if (area > max_area)
+        {
+            idx = i;
+            max_area = area;
+        }
+    }
+
+    return idx;
+}
+
+bool is_good_homography(const cv::Mat &hom, const cv::Rect &rect, const bool &do_morphological_check = true)
+{
+    if (hom.empty())
+    {
+        return false;
+    }
+
+    cv::Mat sub_homo;
+    hom.convertTo(sub_homo, CV_32F);
+
+    cv::Rect roi(0, 0, 2, 2);
+    sub_homo = hom(roi);
+
+    auto valid_diag = sub_homo.at<float>(0, 0) > 0 and sub_homo.at<float>(1, 1) > 0;
+    auto det = cv::determinant(sub_homo);
+    if (valid_diag and det > 0)
+    {
+        // do morphology test
+        if (do_morphological_check)
+        {
+            if (morphological_check(hom, rect) and morphological_check(hom, cv::Rect{0, 0, 640, 640}))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return true;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
+}; // namespace VUtils
+
+#endif //_UTILS_HPP_
---
+++ util/warp.hpp
@@ -0,0 +1,78 @@
+#ifndef _WARP_HPP_
+#define _WARP_HPP_
+
+#include <opencv2/opencv.hpp>
+
+#include "utils.hpp"
+
+static inline void align_rect(const cv::Mat &H, const cv::Rect &rect, cv::Mat &shift, cv::Size &sz, const cv::Size &max_sz = cv::Size())
+{
+    std::vector<cv::Point2f> corners{
+        cv::Point2f{rect.x, rect.y},
+        cv::Point2f{rect.x + rect.width, rect.y},
+        cv::Point2f{rect.x + rect.width, rect.y + rect.height},
+        cv::Point2f{rect.x, rect.y + rect.height}};
+    std::vector<cv::Point2f> warped_corners(4);
+    cv::perspectiveTransform(corners, warped_corners, H);
+    auto _r = cv::boundingRect(warped_corners);
+    sz = _r.size();
+
+    float scale = 1.0;
+    if (max_sz.width)
+    {
+        auto s = (float)max_sz.width / sz.width;
+        if (s < scale)
+        {
+            scale = s;
+        }
+    }
+    if (max_sz.height)
+    {
+        auto s = (float)max_sz.height / sz.height;
+        if (s < scale)
+        {
+            scale = s;
+        }
+    }
+    if (scale != 1.0)
+    {
+        for (auto &p : warped_corners)
+        {
+            p -= cv::Point2f{_r.x, _r.y};
+            p *= scale;
+        }
+        shift = cv::findHomography(corners, warped_corners, cv::noArray(), cv::LMEDS);
+        shift.convertTo(shift, CV_32F);
+        _r = cv::boundingRect(warped_corners);
+        sz = _r.size();
+    }
+    else
+    {
+        shift = cv::Mat::eye(3, 3, CV_32F);
+        shift.at<float>(0, 2) = -_r.x;
+        shift.at<float>(1, 2) = -_r.y;
+        shift = shift * H;
+    }
+}
+
+static inline void warp(const cv::Mat &input, cv::Mat &output, const cv::Mat &H, const cv::Size max_sz = cv::Size())
+{
+    cv::Mat _H;
+    H.convertTo(_H, CV_32F);
+    auto sz = input.size();
+    auto rect = cv::Rect{0, 0, sz.width, sz.height};
+    if (VUtils::is_good_homography(_H, rect))
+    {
+        cv::Mat shift;
+        cv::Size sz;
+        align_rect(_H, rect, shift, sz, max_sz);
+        output = cv::Mat::zeros(sz, input.type());
+        cv::warpPerspective(input, output, shift, sz, cv::INTER_LANCZOS4, cv::BORDER_TRANSPARENT);
+    }
+    else
+    {
+        output.release();
+    }
+}
+
+#endif //_WARP_HPP_
